99467705ac8796f438dae66c30709c28a253b9a3
vpilipets-3 <vpilipets@agilevision.io>
2019-11-20 11:06:33 +0200

    test_c

---
+++ test/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Reptile
+{
+public:
+	int age;
+	string name;
+	float lenght;
+	void Print()
+	{
+		cout << "NAME: " << name << "\nAGE: " << age << "\nLENGHT: "<< lenght << "\n<()()()()()()()()()()()()(  ')-<" << endl;
+	}
+};
+
+int main()
+{
+	float cof = 1.5; // age lenght modifier 
+	Reptile Snake;
+	Snake.age = 10;
+	Snake.name = "Anakonda";
+	Snake.lenght = Snake.age*cof;
+	Snake.Print();
+	cout << "___________________________" << endl;
+	return 0;
+}
